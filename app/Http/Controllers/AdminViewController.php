515520f63030fbf1d79060d417213de47bacd251
Desdes06 <desvira2oo2@gmail.com>
2025-02-11 15:09:30 +0700

    data grafik pengembalian

--- app/Http/Controllers/AdminViewController.php
+++ app/Http/Controllers/AdminViewController.php
@@ -11,14 +11,29 @@ use Illuminate\Http\Request;
 
 class AdminViewController extends Controller
 {
-    public function beranda(){
-        $tanggalSekarang = Carbon::now()->locale('id');
-        $jumlahUser = User::where('role_id', 1)->count();
-        $jumlahpinjam = Pinjam::all()->count();
-        $jumlahbuku = Buku::all()->count();
-        $pengembalian = Pengembalian::all()->count();
-        return view('Admin.Berandaadmin', compact('tanggalSekarang', 'jumlahUser', 'jumlahpinjam', 'jumlahbuku', 'pengembalian'));
+    public function beranda()
+{
+    $tanggalSekarang = Carbon::now()->locale('id');
+    $jumlahUser = User::where('role_id', 1)->count();
+    $jumlahpinjam = Pinjam::count();
+    $jumlahbuku = Buku::count();
+    $pengembalian = Pengembalian::count();
+
+    $pengembalianPerBulan = Pengembalian::selectRaw('MONTH(tanggal_pengembalian) as bulan, COUNT(*) as jumlah')
+        ->whereNotNull('tanggal_pengembalian') 
+        ->groupBy('bulan')
+        ->orderBy('bulan')
+        ->pluck('jumlah', 'bulan')
+        ->toArray();
+
+    $pengembalianPerBulanArray = array_fill(1, 12, 0);
+
+    foreach ($pengembalianPerBulan as $bulan => $jumlah) {
+        $pengembalianPerBulanArray[$bulan] = $jumlah;
     }
+
+    return view('Admin.Berandaadmin', compact('tanggalSekarang', 'jumlahUser', 'jumlahpinjam', 'jumlahbuku', 'pengembalian', 'pengembalianPerBulanArray'));
+}
 
     public function listbuku(){
         return view('Admin.listbuku');
