afde5019ccea4d0ff18cd030f5024e2606c4c138
Desdes06 <desvira2oo2@gmail.com>
2025-03-01 17:27:49 +0700

    update tampilan

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -2,13 +2,13 @@
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
 <form action="{{ $action }}" method="POST" 
-    class="bg-gray-300 shadow-md w-3/5 p-8 rounded-xl flex flex-col space-y-4">
+    class="bg-[#413C88]/20 shadow-md w-3/5 p-8 rounded-xl flex flex-col space-y-4">
     @csrf
     <h1 class="text-3xl font-bold mx-auto">{{ $title }}</h1>
 
     {{ $slot }}
 
-    <button class="bg-black rounded-full p-2 text-white w-28 mx-auto" type="submit">
+    <button class="bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br rounded-full p-2 text-white w-28 mx-auto" type="submit">
         {{ $buttonText }}
     </button>
 </form>
