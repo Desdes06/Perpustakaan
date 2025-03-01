ba1f46933b5a566e9cd4566e11cccc8ad69360ef
Desdes06 <desvira2oo2@gmail.com>
2025-03-01 17:28:07 +0700

    fix bug tampilan

--- resources/views/components/sortirpilih.blade.php
+++ resources/views/components/sortirpilih.blade.php
@@ -63,7 +63,7 @@
                   placeholder="{{ $slot }}"
                   value="{{ request('search') }}"
               />
-              <button type="submit" class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-black rounded-e-md hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              <button type="submit" class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-gray-900 rounded-e-md hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                   <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                   </svg>
