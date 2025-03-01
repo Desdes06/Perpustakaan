3bc1530db2c2b9a7dfeb7dd01be0e18b9aea28c6
Desdes06 <desvira2oo2@gmail.com>
2025-03-01 17:27:29 +0700

    update tampilan footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,27 @@
+
+
+<footer class="bg-[#413C88]">
+    <div class="w-full p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="/User/beranda">
+                <img src="{{ asset('img/logo.png') }}" class="h-8 w-auto" alt="Flowbite Logo" />
+            </a>
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium sm:mb-0 text-gray-400">
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-gray-400 sm:text-center">© 2023 <a href="/User/beranda" class="hover:underline">Perpus</a>. All Rights Reserved.</span>
+    </div>
+</footer>
