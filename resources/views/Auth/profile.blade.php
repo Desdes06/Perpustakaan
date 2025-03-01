266f33534cd9edf60c9bbcd788ccaee043ac628f
Desdes06 <desvira2oo2@gmail.com>
2025-03-01 17:27:03 +0700

    update tampilan dan menambah route ke halaman riwayat

--- resources/views/Auth/profile.blade.php
+++ resources/views/Auth/profile.blade.php
@@ -27,7 +27,7 @@
                     @csrf
                     @method('PUT')
                     <div class="w-full bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-                        {{-- <div class="flex justify-end px-4 pt-4">
+                        <div class="flex justify-end px-4 pt-4">
                             <button id="dropdownButton" data-dropdown-toggle="dropdown" class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5" type="button">
                                 <span class="sr-only">Open dropdown</span>
                                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
@@ -37,11 +37,11 @@
                             <div id="dropdown" class="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700">
                                 <ul class="py-2" aria-labelledby="dropdownButton">
                                     <li>
-                                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Edit</a>
+                                        <a href="/User/riwayat" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Riwayat</a>
                                     </li>
                                 </ul>
                             </div>
-                        </div> --}}
+                        </div>
                         <div class="flex flex-col items-center p-10">
                             <img id="preview-image" 
                                  src="{{ $user->foto ? asset('storage/' . $user->foto) : asset('img/profile.png') }}" 
@@ -53,11 +53,11 @@
                                 <div>
                                     <input type="file" name="foto" id="foto" class="hidden" />
                                     <label for="foto" 
-                                           class="flex items-center justify-center px-4 py-2 text-white bg-gray-400 rounded-lg hover:bg-gray-500 focus:outline-none">
+                                           class="flex items-center justify-center px-4 py-2 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br rounded-lg focus:outline-none">
                                         Edit Foto
                                     </label>
                                 </div>
-                                <a href="/Auth/logout" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Keluar</a>
+                                <a href="/Auth/logout" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300">Keluar</a>
                             </div>
                         </div>
                     </div>
@@ -77,7 +77,7 @@
                             <label for="username" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Username</label>
                         </div>
                         </div>               
-                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <button type="submit" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
                             Simpan Perubahan
                         </button>
                     </div>
