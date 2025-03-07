e35fbc7211f53f4ba4ce12c9c6b3f94b3616ecd4
Desdes06 <desvira2oo2@gmail.com>
2025-03-07 12:49:19 +0700

    verifikasi otp

--- app/Http/Controllers/VerificationController.php
+++ app/Http/Controllers/VerificationController.php
@@ -14,26 +14,22 @@ class VerificationController extends Controller
 
     public function verify(Request $request)
     {
-        // Validasi input
         $request->validate([
             'email' => 'required|email',
             'otp' => 'required|digits:6',
         ]);
 
-        // Cari user berdasarkan email
         $user = User::where('email', $request->email)->where('otp', $request->otp)->first();
 
         if (!$user) {
             return redirect()->back()->with('error', 'Kode OTP salah atau email tidak ditemukan.');
         }
 
-        // Hapus OTP dan verifikasi email
         $user->update([
             'email_verified_at' => now(),
             'otp' => null
         ]);
 
-        // Redirect ke login
         return redirect('/Auth/login')->with('message', 'Verifikasi berhasil! Silakan login.');
     }
 }
